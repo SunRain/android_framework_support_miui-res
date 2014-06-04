149ac3a034fff46529f8a304cd0acbbe090f1bc9
WangGuojian <wanggjghost@gmail.com>
2014-06-04 23:37:35 +0800

    init miui res

---
+++ Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+framework_res_source_path := APPS/framework-res_intermediates/src
+
+LOCAL_SDK_VERSION := current
+			
+LOCAL_PACKAGE_NAME := framework-miui-res
+LOCAL_CERTIFICATE := platform
+
+# Tell aapt to create "extending (non-application)" resource IDs,
+# since these resources will be used by many apps.
+LOCAL_AAPT_FLAGS := -x
+
+LOCAL_MODULE_TAGS := optional
+
+# Install this alongside the libraries.
+LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)
+
+# Create package-export.apk, which other packages can use to get
+# PRODUCT-agnostic resource data like IDs and type definitions.
+LOCAL_EXPORT_PACKAGE_RESOURCES := true
+
+include $(BUILD_PACKAGE)
+
